48fea82e4c0f5b7c61c31274a84ecd63115d2b22
kaktusbezcherepa <eli1starboom@gmail.com>
2025-04-22 16:06:10 +0300

    router pages

---
+++ frontend/src/pages/error/ErrorPage.tsx
@@ -0,0 +1,11 @@
+import styles from './ErrorPage.module.css'
+
+const ErrorPage = () => {
+  return (
+    <div className={styles.error_container}>
+      Ошибка 404
+    </div>
+  )
+}
+
+export default ErrorPage
---
+++ frontend/src/pages/main/MainPage.tsx
@@ -0,0 +1,11 @@
+
+
+const MainPage = () => {
+  return (
+    <div>
+    <p>maon page</p>
+    </div>
+  )
+}
+
+export default MainPage
