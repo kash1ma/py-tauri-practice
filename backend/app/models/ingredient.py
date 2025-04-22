40afb2b61c94e0f3854cdb5064b6de2bfd6efe83
SoulVayne <gigalo1403@yandex.ru>
2025-04-22 22:22:24 +0300

    gitignor

--- backend/app/models/ingredient.py
+++ backend/app/models/ingredient.py
@@ -5,4 +5,4 @@ class Ingredient(Base):
     __tablename__ = "ingredients"
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String)
-    stock_quantity = Column(Integer)
+    stock_quantity = Column(Intege)
