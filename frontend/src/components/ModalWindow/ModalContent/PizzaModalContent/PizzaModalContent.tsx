e4f14f6d248b46c9dc574e02d4f7e648940c823c
kaktusbezcherepa <eli1starboom@gmail.com>
2025-04-25 17:16:53 +0300

    asd

--- frontend/src/components/ModalWindow/ModalContent/PizzaModalContent/PizzaModalContent.tsx
+++ frontend/src/components/ModalWindow/ModalContent/PizzaModalContent/PizzaModalContent.tsx
@@ -3,6 +3,10 @@ import { addToCart } from "../../../../features/cart/cartSlice";
 import { useDispatch } from "react-redux";
 import Button from "../../../../ui/Button/Button";
 import { IPizza } from "../../../../fakedata/data";
+import styles from "./PizzaModalContent.module.css"
+import Input from "../../../../ui/Input/Input";
+import { TypesInput } from "../../../../types/enums/InputEnums";
+import useInput from "../../../../hooks/useInput";
 
 type IPizzaModalContentProps = {
   pizza: IPizza;
@@ -10,17 +14,21 @@ type IPizzaModalContentProps = {
 };
 
 const PizzaModalContent: FC<IPizzaModalContentProps> = ({ pizza, img }) => {
+
+  const quantity = useInput(1)
+
   const dispatch = useDispatch();
 
   return (
-    <div style={{ marginTop: 100 }}>
-      <img src={img} alt="" />
+    <div className={styles.pizza_modal_container}>
+      <img src={img} alt="pizza" />
       {pizza.price}
       {pizza.quanity}
       <Button
         text="добавить в корзину"
         onClick={() => dispatch(addToCart(pizza as any))}
       />
+      <Input onChange={quantity.handleChange} initialValue={quantity.value} type={TypesInput.NUMBER}/>
     </div>
   );
 };
---
+++ frontend/src/types/enums/InputEnums.ts
@@ -0,0 +1,7 @@
+export const enum TypesInput {
+    PASSWORD = "password",
+    NUMBER = "number",
+    TEXT = "text",
+    PHONE = "phone",
+    EMAIL = "email"
+}
