92fd50e13c44b116cdb9e6c1cee3e4a037aa39f4
belov1van <67631064+belov1van@users.noreply.github.com>
2025-04-25 22:38:25 +0300

    fix

--- frontend/src/components/PizzaCard/PizzaCard.tsx
+++ frontend/src/components/PizzaCard/PizzaCard.tsx
@@ -3,7 +3,6 @@ import { IPizza } from "../../fakedata/data";
 import styles from "./PizzaCard.module.css";
 import Button from "../../ui/Button/Button";
 
-
 import Pizza from "../../assets/pizza.jpg"
 import ModalWindow from "../ModalWindow/ModalWindow";
 import PizzaModalContent from "../ModalWindow/ModalContent/PizzaModalContent/PizzaModalContent.js"
@@ -13,24 +12,39 @@ type PizzaCardProps  = {
 };
 
 const PizzaCard: FC<PizzaCardProps> = ({ pizza }) => {
-
-
   const [isOpenModal, setIsOpenModal] = useState(false)
 
   return (
     <div className={styles.pizza_card}>
-    <img loading="lazy" style={{width:180, height:180, marginTop:10}} src={Pizza} alt="pizza" />
-      <p>{pizza.name}</p>
-      <p style={{color: "white"}}>{pizza.decription}</p>
-      <p>От {pizza.price} Рублей</p>
-      <Button
-        text="Выбрать"
-        // onClick={() => dispatch(addToCart(pizza as any))}
-        onClick={() => setIsOpenModal(true)}
-      />
-      {isOpenModal && (<ModalWindow size="large" isOpen={isOpenModal} onClose={() => setIsOpenModal(false)} children={<PizzaModalContent img={Pizza} pizza={pizza}/>}/>)}   
+      <div className={styles.pizza_container}>
+        <div className={styles.image_container}>
+          <img 
+            loading="lazy" 
+            className={styles.pizza_image} 
+            src={Pizza} 
+            alt="pizza" 
+          />
+        </div>
+        <div className={styles.pizza_info}>
+          <p className="info">{pizza.name}</p>
+          <p>{pizza.decription}</p>
+          <p className="info" style={{color: "black", fontWeight: "bold"}}>От {pizza.price} Рублей</p>
+        </div>
+        <Button
+          text="Выбрать"
+          onClick={() => setIsOpenModal(true)}
+          otherButtonStyles={{padding: "20px", width: "100%"}}
+        />
+        {isOpenModal && (
+          <ModalWindow 
+            size="large" 
+            isOpen={isOpenModal} 
+            onClose={() => setIsOpenModal(false)} 
+            children={<PizzaModalContent img={Pizza} pizza={pizza}/>}
+          />
+        )}   
+      </div>
     </div>
-    
   );
 };
 
