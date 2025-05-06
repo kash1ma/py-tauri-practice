1dc25cfc511c671657974738dd505cf7b07799ff
SoulVayne <gigalo1403@yandex.ru>
2025-05-07 02:44:56 +0300

    ingredient fix

--- backend/app/schemas/ingredient_schemas.py
+++ backend/app/schemas/ingredient_schemas.py
@@ -1,11 +1,19 @@
 from pydantic import BaseModel
+from typing import Optional
 
 class IngredientBase(BaseModel):
     name: str
-    stock_quantity: int
 
 class IngredientCreate(IngredientBase):
-    pass
+    stock_quantity: int
+
+class IngredientUpdate(BaseModel):
+    name: Optional[str] = None
+    stock_quantity: Optional[int] = None
 
 class IngredientRead(IngredientBase):
-    name: str
+    id: int
+    stock_quantity: int
+
+    class Config:
+        orm_mode = True
