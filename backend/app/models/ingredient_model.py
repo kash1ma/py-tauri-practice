d2d4403603d40ca2b2dd29e2c22aa1cc14431d78
SoulVayne <gigalo1403@yandex.ru>
2025-05-07 02:54:01 +0300

    delete ingredient

--- backend/app/models/ingredient_model.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import Column, Integer, String
-from app.db.base import Base
-
-class Ingredient(Base):
-    __tablename__ = "ingredient"
-
-    id = Column(Integer, primary_key=True, index=True)
-    name = Column(String, nullable=False, unique=True)
-    stock_quantity = Column(Integer, default=0)
