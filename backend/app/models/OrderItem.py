9713cf1c121cf3b6e6d1f9d30babc2e35c32c53e
SoulVayne <gigalo1403@yandex.ru>
2025-05-06 17:32:12 +0300

    fix name

--- backend/app/models/OrderItem.py
+++ backend/app/models/OrderItem.py
@@ -5,8 +5,8 @@ from app.db.base import Base
 class OrderItem(Base):
     __tablename__ = "order_items"
     id = Column(Integer, primary_key=True, index=True)
-    order_id = Column(Integer, ForeignKey("orders.id"), nullable=False)
-    pizza_id = Column(Integer, ForeignKey("pizzas.id"), nullable=False)
+    order_id = Column(Integer, ForeignKey("order.id"), nullable=False)
+    pizza_id = Column(Integer, ForeignKey("pizza.id"), nullable=False)
     quantity = Column(Integer, nullable=False)
 
     order = relationship("Order", back_populates="items")
