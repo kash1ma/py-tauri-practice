9e30f67a02f0a94faba483e68f0d7aef2be63580
SoulVayne <gigalo1403@yandex.ru>
2025-05-06 16:43:35 +0300

    fix init name

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -1,10 +1,11 @@
 from app.db.base import Base
-from .user import User
-from .pizza import Pizza
-from .ingridient import Ingredient
+
+from .user_model import User
+from .pizza_model import Pizza
+from .ingredient import Ingredient
 from .pizza_ingredient import PizzaIngredient
 from .Order import Order
-from .order_item import OrderItem
+from .OrderItem import OrderItem
 
 __all__ = [
     "Base",
