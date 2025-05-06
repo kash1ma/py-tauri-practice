479511ca8676ed7b076ef286a59e18adbd83c146
kaktusbezcherepa <eli1starboom@gmail.com>
2025-05-07 02:24:13 +0300

    VERY GOOD IDEA

--- frontend/src/hooks/useFetch.tsx
+++ frontend/src/hooks/useFetch.tsx
@@ -1,12 +1,12 @@
 import axios, { AxiosResponse } from "axios";
 import { useEffect, useState } from "react";
 
-const useFetch = (
+function useFetch<T>  (
   url: string,
   type: "post" | "get",
   body?: Record<string, any>
-) => {
-  const [data, setData] = useState<null | AxiosResponse>(null);
+) {
+  const [data, setData] = useState<null | AxiosResponse<T>>(null);
   const [error, setError] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
 
@@ -14,13 +14,13 @@ const useFetch = (
     if (type === "post") {
       axios
         .post(url, body)
-        .then((response: AxiosResponse) => setData(response))
+        .then((response: AxiosResponse<T>) => setData(response))
         .catch((err) => setError(err))
         .finally(() => setIsLoading(false));
     } else {
       axios
         .get(url)
-        .then((response: AxiosResponse) => setData(response))
+        .then((response: AxiosResponse<T>) => setData(response))
         .catch((err) => setError(err))
         .finally(() => setIsLoading(false));
     }
--- frontend/src/pages/main/MainPage.tsx
+++ frontend/src/pages/main/MainPage.tsx
@@ -1,11 +1,14 @@
 import PizzaCard from "../../components/PizzaCard/PizzaCard";
 import pizzas from "../../fakedata/data";
 import styles from "./MainPage.module.css"
+import useFetch from "../../hooks/useFetch";
 
-
+interface Test {
+  name: string
+}
 
 const MainPage = () => {
-  
+  // const { data, isLoading, error} = useFetch<Test>("", "get")
   return (
     <div style={{display:"flex", justifyContent:"center", alignItems:"center", flexDirection: "column", width: "100%"}}>
       <p style={{textAlign: "center", fontSize: 60 , color: "#ea7c69"}}>Каталог</p>
