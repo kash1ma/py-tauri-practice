a9c063b00b89c2b26bc88c8b85dbb550a20f57d9
Komplexe <artval570@gmail.com>
2025-05-07 16:24:57 +0300

    fixOrderItemRout

--- backend/app/api/v1/routes/__init__.py
+++ backend/app/api/v1/routes/__init__.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
 from app.api.v1.routes import auth
-from app.views import pizza_view, user_view, order_view
+from app.views import pizza_view, user_view, order_view, order_item_view
 
 router = APIRouter()
 
@@ -8,4 +8,4 @@ router.include_router(auth.router, prefix="/auth", tags=["auth"])
 router.include_router(pizza_view.router, tags=["pizzas"])
 router.include_router(user_view.router, tags=["users"])
 router.include_router(order_view.router, tags=["orders"])
-router.include_router(order_view.router, tags=["orders_items"])
+router.include_router(order_item_view.router, tags=["order-items"])
