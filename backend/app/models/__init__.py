bcd34d99fdc61821d93b9dddb6d89eda411f5baa
SoulVayne <gigalo1403@yandex.ru>
2025-05-07 04:21:31 +0300

    fix router

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -2,17 +2,12 @@ from app.db.base import Base
 
 from .user_model import User
 from .pizza_model import Pizza
-from .ingredient import Ingredient
-from .pizza_ingredient import PizzaIngredient
-from .Order import Order
-from .OrderItem import OrderItem
+from .order_model import Order
+# from .OrderItem import OrderItem
 
 __all__ = [
     "Base",
     "User",
     "Pizza",
-    "Ingredient",
-    "PizzaIngredient",
-    "Order",
-    "OrderItem",
+    "Order"
 ]
