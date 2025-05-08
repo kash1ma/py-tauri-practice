ab41c41614b7ba6222b18f368c35016c785be834
kash1ma <ayollo.matvey@gmail.com>
2025-05-08 03:11:40 +0300

    fix: fixed some typing errors

--- backend/app/db/session.py
+++ backend/app/db/session.py
@@ -1,3 +1,4 @@
+from typing_extensions import Generator
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm import Session
@@ -8,7 +9,7 @@ engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 # Добавляем функцию get_db
-def get_db() -> Session:
+def get_db() -> Generator[Session, None, None]:
     db = SessionLocal()
     try:
         yield db
